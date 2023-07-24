ab9befc17e969011e2e4219f2ae809f2b85ec0fe
mkhripkov <michaelkhripkov@gmail.com>
2023-07-24 19:46:31 -0400

    Added some boilerplate HTML code

--- typingtest.py
+++ typingtest.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Landing page
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World!"
+    return render_template('home.html')
 
 # About Page
 @app.route('/about')
 def about():
-    return "Hello About Page!"
+    return render_template('about.html')
 
 if __name__ == '__main__': # Allows us to run the script directly with Python
     app.run(debug=True)
