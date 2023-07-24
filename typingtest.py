17fba12ba82586c3466dd689dcba6471ca1fb964
mkhripkov <michaelkhripkov@gmail.com>
2023-07-24 19:34:39 -0400

    Added about page
    Added an about page route. Also allowed the home page to be accessed through "/home".

--- typingtest.py
+++ typingtest.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Landing page
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     return "Hello World!"
+
+# About Page
+@app.route('/about')
+def about():
+    return "Hello About Page!"
 
 if __name__ == '__main__': # Allows us to run the script directly with Python
     app.run(debug=True)
