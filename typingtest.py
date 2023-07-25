26f72bc6edacd8ad7fab92d6b50e7e803088f6d7
mkhripkov <michaelkhripkov@gmail.com>
2023-07-24 20:04:49 -0400

    Added contact us page

--- typingtest.py
+++ typingtest.py
@@ -12,5 +12,10 @@ def home():
 def about():
     return render_template('about.html')
 
+# Contact Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__': # Allows us to run the script directly with Python
     app.run(debug=True)
