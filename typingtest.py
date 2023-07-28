0610b013c6659ebe7b66deaa767f4766f48bedc3
mkhripkov <michaelkhripkov@gmail.com>
2023-07-28 15:55:36 -0400

    Added a proper title system
    Added an if else statement that checks if a title has been passed in or not. If no title has been passed in through python, then the title defaults to 'Typing Test'

--- typingtest.py
+++ typingtest.py
@@ -10,12 +10,12 @@ def home():
 # About Page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 # Contact Page
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title='Contact')
 
 if __name__ == '__main__': # Allows us to run the script directly with Python
     app.run(debug=True)
